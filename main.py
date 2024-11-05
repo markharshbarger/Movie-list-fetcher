f8f57efd3a55eb30a96c78920277d42bcafd38b1
markharshbarger <markharshbarger25@gmail.com>
2024-11-04 19:52:33 -0500

    added app log

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import os
 
+# Path to the directories where the movies are stored
+movie_directory = ["../../../git/movie-dir-1/", "../../../git/movie-dir-2/"]
+
+for directory in movie_directory:
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            print(file)
